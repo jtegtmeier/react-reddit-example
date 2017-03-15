dc34d00e743a11ef2d0cb2e23dac18add801a308
jake tegtmeier <jcbtegtmeier@gmail.com>
2017-03-15 10:12:06 -0700

    second commit after react and npm setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header-screen">
+          Head
+        </div>
+        <div className="main-screen">
+          Main
+          <
+        </div>
+        <div className="footer-screen">
+          Foot
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
